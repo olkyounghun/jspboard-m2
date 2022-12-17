72fc23d9d58ee0ff6bec8cbc95ded81106d2fb35
Lee kyoung hun <ol.kyounghun@gmail.com>
2022-12-17 20:47:27 +0900

    Initial commit

---
+++ src/main/java/com/example/jspboard2/Jspboard2Application.java
@@ -0,0 +1,13 @@
+package com.example.jspboard2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jspboard2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jspboard2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jspboard2/Jspboard2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jspboard2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jspboard2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
