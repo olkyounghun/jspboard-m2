e0b7ae5f1e14171d39ba24f5ca1f799b94465306
Lee kyoung hun <ol.kyounghun@gmail.com>
2022-12-18 22:54:06 +0900

    불편한 홈페이지 시작

--- src/main/java/com/example/jspboard2/controller/JspController.java
+++ src/main/java/com/example/jspboard2/controller/JspController.java
@@ -1,14 +1,30 @@
 package com.example.jspboard2.controller;
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/list")
+@RequestMapping("/")
 public class JspController {
 
-    @GetMapping
+    @GetMapping("/Home")
     public String main(){
-        return "list";
+        return "home";
+    }
+
+    @GetMapping("/search")
+    public String search(){
+        String search = "";
+        switch (search){
+            case "List" : return "list";
+            case "list" : return "list";
+            case "목록" : return "list";
+            case "리스트" : return "list";
+            case "로그인" : return "login";
+            case "login" : return "what";
+            case "Login" : return "what";
+            default : return "search";
+        }
     }
 }
