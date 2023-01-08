a53364cfb1be3abb0864ee8b38f9eaa1b9a91ef4
Lee kyoung hun <ol.kyounghun@gmail.com>
2023-01-08 23:10:54 +0900

    MemberController 설정

--- src/main/java/com/example/jspboard2/controller/MemberController.java
+++ src/main/java/com/example/jspboard2/controller/MemberController.java
@@ -6,24 +6,27 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class MemberController {
 
     private MemberService memberService;
 
     @GetMapping("/signup")
-    public  Member getMembership(@Param("userMember") String userMember,
-                          @Param("userPw") String userPw,
-                          @Param("userName") String userName,
-                          @Param("userGender") Integer userGender,
-                          @Param("userEmail1") String userEmail1,
-                          @Param("userEmail2") String userEmail2,
-                          @Param("emailChk") Integer emailChk) {
+    public Member getMembership(@Param("userMember") String userMember,
+                              @Param("userPw") String userPw,
+                              @Param("userName") String userName,
+                              @Param("userGender") Integer userGender,
+                              @Param("userEmail1") String userEmail1,
+                              @Param("userEmail2") String userEmail2,
+                              @Param("emailChk") Integer emailChk) {
+
+        Member member = new Member();
+        memberService.getMembership(userMember,userPw,userName,userGender,userEmail1,userEmail2,emailChk);
 
 
-
-
-        return Member;
+        return member;
     }
 
 }
