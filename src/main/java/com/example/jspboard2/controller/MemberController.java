3ca48c530e66f334bb5195a3083bd026c8301c3a
Lee kyoung hun <ol.kyounghun@gmail.com>
2023-01-15 22:43:47 +0900

    Gradle 수정

--- src/main/java/com/example/jspboard2/controller/MemberController.java
+++ src/main/java/com/example/jspboard2/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
