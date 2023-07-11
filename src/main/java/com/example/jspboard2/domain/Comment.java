956a0f0f3d49a71dc2fa33e8cf2e15caa8d23f7b
Lee kyoung hun <ol.kyounghun@gmail.com>
2023-07-11 21:38:59 +0900

    덧글기능 추가 작성

---
+++ src/main/java/com/example/jspboard2/domain/Comment.java
@@ -0,0 +1,15 @@
+package com.example.jspboard2.domain;
+
+import java.sql.Date;
+
+public class Comment {
+
+    private Integer id_comment;
+    private String title_comment;
+    private String content_comment;
+    private String user_comment;
+    private Date regdate_commnet;
+    private Date moddate_commnet;
+    private Long id_member;
+
+}
