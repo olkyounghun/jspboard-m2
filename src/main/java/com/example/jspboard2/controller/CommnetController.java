5adf8d1c771afa01b862e0aa8cde90be95b619cd
Lee kyoung hun <ol.kyounghun@gmail.com>
2023-07-13 22:13:12 +0900

    덧글컨트롤러 작성

---
+++ src/main/java/com/example/jspboard2/controller/CommnetController.java
@@ -0,0 +1,17 @@
+package com.example.jspboard2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CommnetController {
+
+
+    public ModelAndView getComment(){
+        ModelAndView mv = new ModelAndView();
+        return mv;
+    }
+
+
+
+}
