058fee5d8d4571f84e41b3df5758ee062aea87e0
Lee kyoung hun <ol.kyounghun@gmail.com>
2023-07-15 23:25:49 +0900

    댓글 작성시 무결성검사 작성

--- src/main/java/com/example/jspboard2/controller/CommnetController.java
+++ src/main/java/com/example/jspboard2/controller/CommnetController.java
@@ -3,22 +3,55 @@ package com.example.jspboard2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class CommnetController {
 
 
-    public ModelAndView getComment(){
+    public ModelAndView getComment(HttpServletRequest request){
         ModelAndView mv = new ModelAndView();
+        HttpSession session = request.getSession();
+        String loginId = String.valueOf(session.getAttribute("loginId"));
+        String loginPw = String.valueOf(session.getAttribute("loginPw"));
+        if ( loginId == null && loginPw == null) {
+            mv.addObject("error","loginFail");
+            mv.addObject("errorMessage", "로그인정보가 없습니다.");
+            mv.addObject("errorMove","/login");
+            mv.setViewName("error");
+        }
+
         return mv;
     }
 
-    public ModelAndView modifyComment(){
+    public ModelAndView modifyComment(HttpServletRequest request){
         ModelAndView mv = new ModelAndView();
+        HttpSession session = request.getSession();
+        String loginId = String.valueOf(session.getAttribute("loginId"));
+        String loginPw = String.valueOf(session.getAttribute("loginPw"));
+        if ( loginId == null && loginPw == null) {
+            mv.addObject("error","loginFail");
+            mv.addObject("errorMessage", "로그인정보가 없습니다.");
+            mv.addObject("errorMove","/login");
+            mv.setViewName("error");
+        }
+
         return mv;
     }
 
-    public ModelAndView deleteComment(){
+    public ModelAndView deleteComment(HttpServletRequest request){
         ModelAndView mv = new ModelAndView();
+        HttpSession session = request.getSession();
+        String loginId = String.valueOf(session.getAttribute("loginId"));
+        String loginPw = String.valueOf(session.getAttribute("loginPw"));
+        if ( loginId == null && loginPw == null) {
+            mv.addObject("error","loginFail");
+            mv.addObject("errorMessage", "로그인정보가 없습니다.");
+            mv.addObject("errorMove","/login");
+            mv.setViewName("error");
+        }
+
         return mv;
     }
 
